c35362d1cadd1420539d778dd0c12037973ac787
Yeldar <krutnut@gmail.com>
2025-07-17 09:42:50 +0500

    Initial commit

---
+++ src/test/java/com/cloderno/jwt_auth_1/JwtAuth1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloderno.jwt_auth_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuth1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
